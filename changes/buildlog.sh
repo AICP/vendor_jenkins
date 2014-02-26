0462e7b194360521441dfa3d97717a9a3d5cc935
Zips <hcolmenares@gmail.com>
2014-02-26 12:12:05 -0500

    Modify Change Log
    Change-Id: Ie65b736c3885261922088f9eb356c814e22bd7f0

--- changes/buildlog.sh
+++ changes/buildlog.sh
@@ -1,30 +1,12 @@
-if [ -z "$AICP_BUILD" ]; then
-  ## Use jenkins' variable
-  AICP_BUILD=$LUNCH
-fi
-
-MYPATH=$(dirname $0)
 export CHANGESPATH=$WORKSPACE/$REPO_BRANCH/archive/CHANGES.txt
-rm $CHANGESPATH 2>/dev/null
+export CHANGESFULLPATH=$WORKSPACE/$REPO_BRANCH/CHANGELOGS/$LUNCH.txt
 
-prevts=
-for ts in `python2 $MYPATH/getdates.py $AICP_BUILD | sort -rn`; do
-
-export ts
+export ts=$1
 (echo "==================================="
-echo -n "Since ";date -u -d @$ts 
+echo "Since $(LANG=en_US date -u -d @$ts) to $(LANG=en_US date)" 
 echo "==================================="
-if [ -z "$prevts" ]; then
-  repo forall -c 'L=$(git log --oneline --since $ts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts; fi' | tee >(wc -l >  $WORKSPACE/$REPO_BRANCH/changecount)
-else
-  repo forall -c 'L=$(git log --oneline --since $ts --until $prevts -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --oneline --since $ts --until $prevts; fi'
-fi
-echo) >> $CHANGESPATH
-export prevts=$ts
+repo forall -c 'L=$(git log --oneline --since $ts --grep="[log]" -n 1); if [ "n$L" != "n" ]; then echo; echo "   * $REPO_PATH"; git log --format=%s --since $ts --grep="[log]"; fi'
+echo) > $CHANGESPATH
 
-done
-
-if [ -z "$prevts" ]; then
-  rm -f  $WORKSPACE/$REPO_BRANCH/changecount
-  echo "This is the first AICP build of this type for device $CM_BUILD" >> $CHANGESPATH
-fi
+echo | cat $CHANGESPATH - $CHANGESFULLPATH > temp.changes
+mv temp.changes $CHANGESFULLPATH
